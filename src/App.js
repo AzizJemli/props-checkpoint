f3e396b561dd14f190288d4fc74cff75af65e49a
Aziz <jemliabdlaziz@gmail.com>
2021-11-17 15:32:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Component from './Profile/Component.js';
 
 function App() {
+ 
+  const cardContainer = {
+    backgroundColor: "#753188",
+    borderRadius: "10px",
+    boxShadow: "0px 10px 20px -10px rgba(0, 0, 0, 0.75)",
+    color: "#b3b8cd",
+    paddingTop: "30px",
+    position: "relative",
+    width: "350px",
+    maxWidth: "100%",
+    textAlign: "center",
+  };
+
+  const circle = {
+    border: "2px solid #fff",
+    borderRadius: "50%",
+    padding: "7px",
+    height:"120px",
+    width:"120px"
+  };
+
+  const App = {
+    backgroundColor: "#B983FF",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+
+    minHeight: "100vh",
+    margin: "0",
+  };
+
+
+
+  
+     let handleName=()=>{
+       alert('Aziz Jemli');
+     };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={App}>
+      <div style={cardContainer}>
+        <Component name="Aziz Jemli" data={handleName} pro="Fullstack Developer"  bio="I'm a freelancer,gamer and i love football" >
+          <img
+            src="./profilepic.jpg"
+            alt="profile pic"
+            style={circle}
+          /></Component>
+      </div>
     </div>
   );
 }
